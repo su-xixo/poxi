18a3a954237c50f6b58a7c85f46d96de057e6623
su-xixo <suraj.vishwakarma.01@yandex.com>
2025-03-02 23:09:11 +0530

    jq essential command

--- src/install.sh
+++ src/install.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-# install package function
+ROOT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+UTILS=$ROOT_DIR/src/utils.sh
+source $UTILS
+
 install_pkg(){
-    # check_json_file
-    echo "installing packages"
-    # ==> get list of packages
+    if [ ${#} -eq 0 ]; then
+        # installation with fzf
+        # ==> get list of packages
+        local packages=($(get_all_packages))
+        echo ${#packages[@]}
+        for pkg in ${packages[@]}; do
+            result="$(simmulate_install pkg)" && echo 0 || echo 1
+            if [ $result -eq 0 ]; then
+                jq '.installed | \
+                if index($pkg) == null then \
+                    .+=["$pkg"] \
+                else \
+                    empty
+                end' $PKG_JSON_FILE
+            fi
+        done
+        # $PERU -S $packages # package installer
     
+    else
+        # instllation without fzf
+        local packages="$@"
+        echo $packages
+    fi
+
+
 }
-# install_pkg
+install_pkg "$@"
+
---
+++ src/tempCodeRunnerFile.sh
@@ -0,0 +1,28 @@
+ROOT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+UTILS=$ROOT_DIR/src/utils.sh
+source $UTILS
+pkg="pk1"
+
+jq --arg pkg "$pkg" '
+  if .installed | index($pkg) == null then
+    .installed += [$pkg]
+  else
+    .
+  end
+' $PKG_JSON_FILE | sponge $PKG_JSON_FILE
+
+# jq --arg pkg "$pkg" 'del(.installed[.installed|index($pkg)])' $PKG_JSON_FILE
+
+a=$(jq -r '.required.editor[], .required.terminal[]' $PKG_JSON_FILE)
+b=($a)
+echo ${b[@]}
+
+# jq -r '.required | .. | strings' $PKG_JSON_FILE
+a=$(jq -r '.required | to_entries | map(.value) | flatten | unique' $PKG_JSON_FILE)
+b=($a)
+echo ${#b[@]}
+
+jq '.' $PKG_JSON_FILE
+
+
+
