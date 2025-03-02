ee89cfd5ab8eb1b4f7964f5fcb3152d2e38740fe
su-xixo <suraj.vishwakarma.01@yandex.com>
2025-03-02 23:43:21 +0530

    jq recersive unique

--- src/tempCodeRunnerFile.sh
+++ src/tempCodeRunnerFile.sh
@@ -22,7 +22,8 @@ a=$(jq -r '.required | to_entries | map(.value) | flatten | unique' $PKG_JSON_FI
 b=($a)
 echo ${#b[@]}
 
-jq '.' $PKG_JSON_FILE
+# jq 'walk(if type == "array" then unique else . end)' $PKG_JSON_FILE
+
 
 
 
