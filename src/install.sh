25e9422c5b127eaee0ddb1b5eac1107ce293cfc9
su-xixo <suraj.vishwakarma.01@yandex.com>
2025-03-04 09:16:48 +0530

    install with add to json

--- src/install.sh
+++ src/install.sh
@@ -4,32 +4,21 @@ ROOT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 UTILS=$ROOT_DIR/src/utils.sh
 source $UTILS
 
-install_pkg(){
-    if [ ${#} -eq 0 ]; then
-        # installation with fzf
-        # ==> get list of packages
-        local packages=($(get_all_packages))
-        echo ${#packages[@]}
-        for pkg in ${packages[@]}; do
-            result="$(simmulate_install pkg)" && echo 0 || echo 1
-            if [ $result -eq 0 ]; then
-                jq '.installed | \
-                if index($pkg) == null then \
-                    .+=["$pkg"] \
-                else \
-                    empty
-                end' $PKG_JSON_FILE
-            fi
-        done
-        # $PERU -S $packages # package installer
-    
-    else
-        # instllation without fzf
-        local packages="$@"
-        echo $packages
+function add_to_json {
+    local pkg=$1
+    local check=$(jq --arg pkg "$pkg" '.installed|any(.==$pkg)' $PKG_JSON_FILE)
+    printf "check is: %s\n" "$check"
+    if [ $check == 'false' ]; then
+        jq --arg pkg "$pkg" '.installed += [$pkg]' $PKG_JSON_FILE | sponge $PKG_JSON_FILE
     fi
-
-
 }
-install_pkg "$@"
+
+install_pkg(){
+    local pkgs=($@)
+    echo "available packages to install: ${pkgs[@]}"
+    for pkg in ${pkgs[@]}; do
+        add_to_json $pkg
+    done
+}
+install_pkg pkg1 pkg2 pkg3 pkg4 
 
