bc323a9f922405d47caa57cd0eeb968b1488a61b
su-xixo <suraj.vishwakarma.01@yandex.com>
2025-03-04 16:33:23 +0530

    log non installed done

--- src/install.sh
+++ src/install.sh
@@ -12,27 +12,50 @@ function add_to_json {
         jq --arg pkg "$pkg" '.installed += [$pkg]' $PKG_JSON_FILE | sponge $PKG_JSON_FILE
     fi
 }
+function log_non_installed {
+    local pkg=$1
+    local file=".temp.json"
+    local new_object=$(printf '{"name": "%s"}' $pkg)
+    echo "logged pkg is: $new_object"
+    if [ ! -f "$file" ] || [ ! -s "$file" ]; then # no file or empty
+        echo "{}" > $file
+    fi
+    jq --arg pkg "$pkg" '.not_installed = (
+        if has("not_installed") then
+            if .not_installed | type == "array" then
+                .not_installed | .+= [$pkg] | unique | sort
+            else
+                .not_installed
+            end
+        else
+            [] | .+=[$pkg]
+        end
+        )' $file > temp.json && mv temp.json $file
+}
+
 
 install_pkg(){
     local pkgs=($@)
     if [ ${#pkgs[@]} -eq 0 ]; then
         pkgs+=($(get_all_packages))
     fi
-    # echo "length of pkgs array is ${#pkgs[@]} and array is ${pkgs[@]}" 
-    # return
-    echo "available packages to install: ${pkgs[@]}"
-    # TODO: get oneliner for is_installed
-    simmulate_install ${pkgs[@]}
-    is_installed=$(echo $?)
-    echo "exit code for install command: $is_installed"
-    if test $is_installed -eq 0; then
-        
-    fi
-    
-    return
+
     for pkg in ${pkgs[@]}; do
-        add_to_json $pkg
+        # TODO: get oneliner for is_installed
+        simmulate_install $pkg && is_installed=$? || is_installed=$?
+        # simmulate_install $pkg
+        # is_installed=$(echo $?)
+        if test $is_installed -eq 0; then
+            # echo "adding $pkg into json file"
+            # # add_to_json $pkg
+            echo "$pkg not installed"
+            jq --arg pkg "$pkg" '.+={"name": $pkg}' .temp.json | sponge .temp.json
+        else
+            echo "$pkg not installed"
+            jq --arg pkg "$pkg" '.+={"name": $pkg}' >> .temp.json
+        fi
+        
     done
 }
-install_pkg pkg1 pkg2 pkg3
+install_pkg pkg1 pkg2 pkg4
 
