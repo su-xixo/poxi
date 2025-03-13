3ad5230d02b2c4f0aae953fa158e87aac4896191
su-xixo <suraj.vishwakarma.01@yandex.com>
2025-03-13 16:30:44 +0530

    install function added

--- install.sh
+++ install.sh
@@ -12,14 +12,27 @@ cyan="\e[36m"
 white="\e[37m"
 reset="\e[0m"
 
-URL="https://example.com/path/to/your_script.sh"
+PKGNAME="poxi"
+URL="https://github.com/su-xixo/poxi.git"
+SOURCE_DIR="/usr/local/share/$PKGNAME"
+SOURCE_DIR="$HOME/.local/share/$PKGNAME"
+
 INSTALL_DIR="/usr/local/bin"
-TEMP_DIR=$(mktemp -d)
+# TEMP_DIR=$(mktemp -d)
+echo $TEMP_DIR
 USER=$(whoami)
 declare -A DEPENDENCIES=(
     ['base']='curl jq sponge fzf'
     ['aur']='yay paru'
 )
+
+# # check for root user
+# if [[ ! "$USER" == 'root' || ! $EUID -eq 0 ]]; then
+#     echo -e "${red} WARNING:${reset} sudo privileges is required to run the script" 
+#     exit 1
+# fi
+
+
 
 function check_depds {
     echo -e "${yellow} Checking base dependencies${reset}"
@@ -38,4 +51,41 @@ function check_depds {
         fi
     done
 }
-check_depds
+
+# check_depds
+function install {
+    # 1. check if already installed
+    if command -v "$PKGNAME" >/dev/null 2>&1; then
+        echo -e "${cyan} package already present${reset}"
+        # exit 1
+    fi
+    # 2. download package
+    echo -e "${yellow} Downloading package...${reset}"
+    git clone --branch "$(git ls-remote --tags --sort="v:refname" $URL | tail -n1 | sed 's/.*\///; s/\^{}//')" --single-branch $URL $TEMP_DIR
+    
+    if [[ $? -ne 0 ]]; then
+        echo -e "${red} Failed to download script${reset}"
+        rm -rf "$TEMP_DIR"
+        exit 1
+    fi
+    # 3. install package
+    echo -e "${yellow} Installing script...${reset}"
+    if [ ! -d "$SOURCE_DIR" ]; then
+        mkdir -p "$SOURCE_DIR"
+    fi
+    cp -r "$TEMP_DIR" "$SOURCE_DIR"
+    ln -s $SOURCE_DIR/$PKGNAME/$PKGNAME $INSTALL_DIR/$PKGNAME
+    echo -e "${green} Script installed successfully to $INSTALL_DIR/$PKGNAME${reset}"
+    rm -rf "$TEMP_DIR"
+
+
+}
+function remove {
+    echo
+}
+
+
+{
+    # check_depds
+    install
+}
